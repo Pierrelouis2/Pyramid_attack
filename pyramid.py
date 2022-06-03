183f9048d9b5ce492d7239b31ef02c11d2b0451e
Adrien Dalbeigue <adrien.dalbeigue@cpe.fr>
2022-06-03 11:53:51 +0200

    pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,11 @@
+import OpenGL.GL as GL
+import pyrr
+import numpy as np
+import cpe3d as cpe
+
+class Pyramid(object):
+    def __init__(self, vao, nb_triangle, program, texture):
+        super().__init__(vao, nb_triangle, program, texture)
+
+    def spawn(self):
+        self.proba = 0.1 
