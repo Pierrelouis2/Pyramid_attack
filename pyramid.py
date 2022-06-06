7d723082bde6bb0966af972b87fa471159b6619f
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2022-06-06 20:52:31 +0200

    ok

--- pyramid.py
+++ pyramid.py
@@ -6,7 +6,7 @@ class Pyramid(Entity.Entity):
     #    super().__init__(vao, nb_triangle, program, texture)
     def mouvement(self,humain) :
         
-        vect =[humain.object.transformation.translation.x - self.object.transformation.translation.x,0,humain.object.transformation.translation.z  - self.object.transformation.translation.z]
+        vect =[humain.object.transformation.translation.x - self.object.transformation.translation.x, 0, humain.object.transformation.translation.z  - self.object.transformation.translation.z]
         vect_norm = [float(i)/sum(vect) for i in vect]
         mov = [i*0.005 for i in vect_norm]
         self.object.transformation.translation.x += mov[0]
